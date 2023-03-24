fb005e8a71096f24f393b13e0ff22e55e8bac2af
Luca Guidi <me@lucaguidi.com>
2023-03-24 04:11:17 +0100

    Destination paths (#2)
    * Destination paths
    
    * Create manifest
    
    * Refactoring: `HanamiEsbuildOptions` -> `HanamiEsbuildPluginOptions`
    
    * Refactoring: extract `calulateSourcePath` and `calulateDestinationPath`

--- test/hanami-esbuild.test.ts
+++ test/hanami-esbuild.test.ts
@@ -17,6 +17,7 @@ async function createTestEnvironment() {
   await fs.ensureDir(path.join(dest, 'app/assets/javascripts'));
   await fs.ensureDir(path.join(dest, 'app/assets/images'));
   await fs.ensureDir(path.join(dest, 'slices/admin/assets/javascripts'));
+  await fs.ensureDir(path.join(dest, 'slices/metrics/assets/javascripts'));
   await fs.ensureDir(path.join(dest, 'public'));
 
   process.chdir(dest);
@@ -40,31 +41,33 @@ describe('hanamiEsbuild', () => {
   test('copies assets from app/assets to public/assets and generates a manifest file', async () => {
     const entryPoint1 = path.join(dest, 'app/assets/javascripts/index.js');
     const entryPoint2 = path.join(dest, 'slices/admin/assets/javascripts/index.js');
+    const entryPoint3 = path.join(dest, 'slices/metrics/assets/javascripts/app.ts');
     await fs.writeFile(entryPoint1, "console.log('Hello, World!');");
     await fs.writeFile(entryPoint2, "console.log('Hello, Admin!');");
+    await fs.writeFile(entryPoint3, "console.log('Hello, Metrics!');");
 
     const entrypoints: Record<string, string> = {}
 
-    // Normalize paths for entrypoints.
-    ;[entryPoint1, entryPoint2].map((str) => {
-      let modifiedPath = str.replace(/(app\/assets\/javascripts\/|slices\/(.*\/)assets\/javascripts\/)/, "$2")
-      const relativePath = path.relative(dest, modifiedPath)
+      // Normalize paths for entrypoints.
+      ;[entryPoint1, entryPoint2, entryPoint3].map((str) => {
+        let modifiedPath = str.replace(/(app\/assets\/javascripts\/|slices\/(.*\/)assets\/javascripts\/)/, "$2")
+        const relativePath = path.relative(dest, modifiedPath)
 
-      const { dir, name } = path.parse(relativePath)
+        const { dir, name } = path.parse(relativePath)
 
-      if (dir) {
-        modifiedPath = dir + path.sep + name
-      } else {
-        modifiedPath = name
-      }
-      entrypoints[modifiedPath] = str
-    })
+        if (dir) {
+          modifiedPath = dir + path.sep + name
+        } else {
+          modifiedPath = name
+        }
+        entrypoints[modifiedPath] = str
+      })
 
-    console.log(entrypoints)
     const config: Partial<BuildOptions> = {
       bundle: true,
       outdir: outDir,
       loader: loader,
+      absWorkingDir: dest,
       logLevel: "silent",
       minify: true,
       sourcemap: true,
@@ -82,7 +85,7 @@ describe('hanamiEsbuild', () => {
       // },
     });
 
-    execSync("tree .", {stdio: "inherit"})
+    // execSync("tree .", {stdio: "inherit"})
 
     // FIXME: this path should take into account the file hashing in the file name
     const appAsset = globSync(path.join('public/assets/index-*.js'))[0]
@@ -90,19 +93,27 @@ describe('hanamiEsbuild', () => {
     expect(appAssetExists).toBe(true);
 
     // FIXME: this path should take into account the file hashing in the file name
-    const sliceAsset = globSync(path.join('public/assets/admin/index-*.js'))[0];
-    const sliceAssetExists = await fs.pathExists(sliceAsset);
-    expect(sliceAssetExists).toBe(true);
+    const sliceAsset1 = globSync(path.join('public/assets/admin/index-*.js'))[0];
+    const sliceAssetExists1 = await fs.pathExists(sliceAsset1);
+    expect(sliceAssetExists1).toBe(true);
 
-    // const manifestExists = await fs.pathExists(path.join(dest, 'public/assets.json'));
-    // expect(manifestExists).toBe(true);
+    // FIXME: this path should take into account the file hashing in the file name
+    const sliceAsset2 = globSync(path.join('public/assets/metrics/app-*.js'))[0];
+    const sliceAssetExists2 = await fs.pathExists(sliceAsset2);
+    expect(sliceAssetExists2).toBe(true);
+
+    const manifestExists = await fs.pathExists(path.join(dest, 'public/assets.json'));
+    expect(manifestExists).toBe(true);
 
     // Read and parse the manifest file
-    // const manifestContent = await fs.readFile(path.join(dest, 'public/assets.json'), 'utf-8');
-    // const manifest = JSON.parse(manifestContent);
+    const manifestContent = await fs.readFile(path.join(dest, 'public/assets.json'), 'utf-8');
+    const manifest = JSON.parse(manifestContent);
 
-    // // Check if the manifest contains the correct file paths
-    // expect(manifest).toEqual({
-    // });
+    // Check if the manifest contains the correct file paths
+    expect(manifest).toEqual({
+      "admin/index.js": "/assets/admin/index-YMWJCFAK.js",
+      "index.js": "/assets/index-A3EJVGR4.js",
+      "metrics/app.js": "/assets/metrics/app-62A4ZWTV.js",
+    });
   });
 });
