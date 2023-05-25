b83f75f1d9e963ea3ea14cb6b6a40c127fb2e081
Luca Guidi <me@lucaguidi.com>
2023-05-25 06:46:19 +0200

    Add `dist` do source control

---
+++ dist/hanami-esbuild-plugin.js
@@ -0,0 +1,133 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaults = void 0;
+const fs_extra_1 = __importDefault(require("fs-extra"));
+const path_1 = __importDefault(require("path"));
+const node_crypto_1 = __importDefault(require("node:crypto"));
+const URL_SEPARATOR = '/';
+exports.defaults = {
+    root: '',
+    publicDir: 'public',
+    destDir: path_1.default.join('public', 'assets'),
+    manifestPath: path_1.default.join('public', 'assets.json'),
+    sriAlgorithms: [],
+};
+const hanamiEsbuild = (options = { ...exports.defaults }) => {
+    return {
+        name: 'hananmi-esbuild',
+        setup(build) {
+            build.initialOptions.metafile = true;
+            options.root = options.root || process.cwd();
+            const manifest = path_1.default.join(options.root, options.manifestPath);
+            const externalDirs = build.initialOptions.external || [];
+            build.onEnd(async (result) => {
+                const outputs = result.metafile?.outputs;
+                const assetsManifest = {};
+                const calulateSourceUrl = (str) => {
+                    return normalizeUrl(str).replace(/\/assets\//, '').replace(/-[A-Z0-9]{8}/, '');
+                };
+                const calulateDestinationUrl = (str) => {
+                    return normalizeUrl(str).replace(/public/, '');
+                };
+                const normalizeUrl = (str) => {
+                    return str.replace(/[\\]+/, URL_SEPARATOR);
+                };
+                const calculateSubresourceIntegrity = (algorithm, path) => {
+                    const content = fs_extra_1.default.readFileSync(path, 'utf8');
+                    const hash = node_crypto_1.default.createHash(algorithm).update(content).digest('base64');
+                    return `${algorithm}-${hash}`;
+                };
+                // Inspired by https://github.com/evanw/esbuild/blob/2f2b90a99d626921d25fe6d7d0ca50bd48caa427/internal/bundler/bundler.go#L1057
+                const calculateHash = (hashBytes) => {
+                    const hash = node_crypto_1.default.createHash('sha256').update(hashBytes).digest('hex');
+                    return hash.slice(0, 8).toUpperCase();
+                };
+                function extractEsbuildInputs(inputData) {
+                    const inputs = {};
+                    for (const key in inputData) {
+                        const entry = inputData[key];
+                        if (entry.inputs) {
+                            for (const inputKey in entry.inputs) {
+                                inputs[inputKey] = true;
+                            }
+                        }
+                    }
+                    return inputs;
+                }
+                const copyAsset = (srcPath, destPath) => {
+                    if (fs_extra_1.default.existsSync(destPath)) {
+                        const srcStat = fs_extra_1.default.statSync(srcPath);
+                        const destStat = fs_extra_1.default.statSync(destPath);
+                        if (srcStat.mtimeMs <= destStat.mtimeMs) {
+                            // File already exists and is up-to-date, skip copying
+                            return false;
+                        }
+                    }
+                    if (!fs_extra_1.default.existsSync(path_1.default.dirname(destPath))) {
+                        fs_extra_1.default.mkdirSync(path_1.default.dirname(destPath), { recursive: true });
+                    }
+                    fs_extra_1.default.copyFileSync(srcPath, destPath);
+                    return true;
+                };
+                const processAssetDirectory = (pattern, inputs) => {
+                    const dirPath = path_1.default.dirname(pattern);
+                    const files = fs_extra_1.default.readdirSync(dirPath);
+                    const assets = [];
+                    files.forEach((file) => {
+                        const srcPath = path_1.default.join(dirPath, file);
+                        // Skip if the file is already processed by esbuild
+                        if (inputs.hasOwnProperty(srcPath)) {
+                            return;
+                        }
+                        const fileHash = calculateHash(fs_extra_1.default.readFileSync(srcPath));
+                        const fileExtension = path_1.default.extname(srcPath);
+                        const baseName = path_1.default.basename(srcPath, fileExtension);
+                        const destFileName = `${baseName}-${fileHash}${fileExtension}`;
+                        const destPath = path_1.default.join(options.destDir, path_1.default.relative(dirPath, srcPath).replace(file, destFileName));
+                        if (fs_extra_1.default.lstatSync(srcPath).isDirectory()) {
+                            assets.push(...processAssetDirectory(destPath, inputs));
+                        }
+                        else {
+                            const copied = copyAsset(srcPath, destPath);
+                            if (copied) {
+                                assets.push(destPath);
+                            }
+                        }
+                    });
+                    return assets;
+                };
+                if (typeof outputs === 'undefined') {
+                    return;
+                }
+                const inputs = extractEsbuildInputs(outputs);
+                const copiedAssets = [];
+                externalDirs.forEach((pattern) => {
+                    copiedAssets.push(...processAssetDirectory(pattern, inputs));
+                });
+                const assetsToProcess = Object.keys(outputs).concat(copiedAssets);
+                for (const assetToProcess of assetsToProcess) {
+                    if (assetToProcess.endsWith('.map')) {
+                        continue;
+                    }
+                    const destinationUrl = calulateDestinationUrl(assetToProcess);
+                    const sourceUrl = calulateSourceUrl(destinationUrl);
+                    var asset = { url: destinationUrl };
+                    if (options.sriAlgorithms.length > 0) {
+                        asset.sri = [];
+                        for (const algorithm of options.sriAlgorithms) {
+                            const subresourceIntegrity = calculateSubresourceIntegrity(algorithm, assetToProcess);
+                            asset.sri.push(subresourceIntegrity);
+                        }
+                    }
+                    assetsManifest[sourceUrl] = asset;
+                }
+                // Write assets manifest to the destination directory
+                await fs_extra_1.default.writeJson(manifest, assetsManifest, { spaces: 2 });
+            });
+        },
+    };
+};
+exports.default = hanamiEsbuild;
---
+++ dist/hanami-esbuild.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const glob_1 = require("glob");
+const node_process_1 = require("node:process");
+const esbuild_1 = __importDefault(require("esbuild"));
+const hanami_esbuild_plugin_1 = __importDefault(require("./hanami-esbuild-plugin"));
+const hanami_esbuild_plugin_2 = require("./hanami-esbuild-plugin");
+const parseArgs = (args) => {
+    const result = {};
+    args.slice(2).forEach((arg) => {
+        const [key, value] = arg.replace(/^--/, '').split('=');
+        result[key] = value;
+    });
+    return result;
+};
+const mapEntryPoints = (entryPoints) => {
+    const result = {};
+    entryPoints.forEach((entryPoint) => {
+        let modifiedPath = entryPoint.replace(entryPointsMatcher, "$2");
+        const relativePath = path_1.default.relative(dest, modifiedPath);
+        const { dir, name } = path_1.default.parse(relativePath);
+        if (dir) {
+            modifiedPath = path_1.default.join(dir, name);
+        }
+        else {
+            modifiedPath = name;
+        }
+        result[modifiedPath] = entryPoint;
+    });
+    return result;
+};
+const externalEsbuildDirectories = () => {
+    const assetDirsPattern = [
+        path_1.default.join("app", "assets", "*"),
+        path_1.default.join("slices", "*", "assets", "*"),
+    ];
+    const excludeDirs = ['javascripts', 'stylesheets'];
+    try {
+        const dirs = (0, glob_1.globSync)(assetDirsPattern, { nodir: false });
+        const filteredDirs = dirs.filter((dir) => {
+            const dirName = dir.split(path_1.default.sep).pop();
+            return !excludeDirs.includes(dirName);
+        });
+        return filteredDirs.map((dir) => path_1.default.join(dir, "*"));
+    }
+    catch (err) {
+        console.error('Error listing external directories:', err);
+        return [];
+    }
+};
+const args = parseArgs(node_process_1.argv);
+const dest = process.cwd();
+const outDir = path_1.default.join(dest, 'public', 'assets');
+const loader = {
+    '.tsx': 'tsx',
+    '.ts': 'ts',
+    '.js': 'js',
+    '.jsx': 'jsx',
+    '.json': 'json',
+    '.png': 'file',
+    '.jpg': 'file',
+    '.jpeg': 'file',
+    '.gif': 'file',
+    '.svg': 'file',
+    '.woff': 'file',
+    '.woff2': 'file',
+    '.otf': 'file',
+    '.eot': 'file',
+    '.ttf': 'file',
+};
+const entryPointExtensions = "app.{js,ts,mjs,mts,tsx,jsx}";
+const entryPoints = (0, glob_1.globSync)([
+    path_1.default.join("app", "assets", "javascripts", "**", entryPointExtensions),
+    path_1.default.join("slices", "*", "assets", "javascripts", "**", entryPointExtensions),
+]);
+// FIXME: make cross platform
+const entryPointsMatcher = /(app\/assets\/javascripts\/|slices\/(.*\/)assets\/javascripts\/)/;
+const mappedEntryPoints = mapEntryPoints(entryPoints);
+const externalDirs = externalEsbuildDirectories();
+var sriAlgorithms = [];
+if (args['sri']) {
+    sriAlgorithms = args['sri'].split(',');
+}
+const options = { ...hanami_esbuild_plugin_2.defaults, sriAlgorithms: sriAlgorithms };
+const config = {
+    bundle: true,
+    outdir: outDir,
+    absWorkingDir: dest,
+    loader: loader,
+    external: externalDirs,
+    logLevel: "silent",
+    minify: true,
+    sourcemap: true,
+    entryNames: "[dir]/[name]-[hash]",
+    plugins: [(0, hanami_esbuild_plugin_1.default)(options)],
+};
+// FIXME: add `await` to esbuild.build
+esbuild_1.default.build({
+    ...config,
+    entryPoints: mappedEntryPoints,
+}).catch(err => {
+    console.log(err);
+    process.exit(1);
+});
